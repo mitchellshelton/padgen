a4e5a2fe44761827fe949d892bd07301cb6186d8
Mitchell Shelton <info@mitchellshelton.com>
2015-10-30 22:28:14 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+/*
+
+1 kick 1
+2 kick 2
+3 snare 1
+4 snare 2
+5 closed hh
+6 bass
+7 melody 1
+8 melody 2
+9 melody 3
+10 open hh
+11 backing
+12 noise 1
+13 noise 2
+14 noise 3
+15 noise 4
+16 noise 5
+
+position
+sound
+multiplier
+
+pad
+grid
+pattern
+
+120 bpm x 3 minutes = 360 Steps / 4 steps in a measure = 90 Measures
+135 bpm x 3 minutes = 405 Steps / 4 steps in a measure = 101.25 Measures
+
+405 Steps / grid size = x loops
+
+*/
+
+<?php
+
+
+
+?>
